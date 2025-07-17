1b7ebcd93fe055ee252dfc7f3989b8268533c9b3
Fernando Herrero <herrerofernando.94@gmail.com>
2025-07-17 19:24:02 +0200

    feat(Header): add toggle button to show/hide summary

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,41 @@
+import { useState } from "react";
 import "./Header.css";
 
 export const Header = ({ personalInfo, summary }) => {
 	const { fullName, title, location, email, phone, linkedin, github } = personalInfo;
+	const [hideSummary, setHideSummary] = useState(false);
+
+	const onClickSummary = () => {
+		setHideSummary((prev) => !prev);
+	};
 
 	return (
 		<section className="header-container">
-			<h1 className="header-name">{fullName}</h1>
+			<div className="header-name-content">
+				<h1 className="header-name">{fullName}</h1>
 
-			<div className="header-content">
-				<p>{title}</p>
-				<p>{location}</p>
-				<a href={`mailto:${email}`}>{email}</a>
-				<a href={`tel:${phone}`}>+{phone}</a>
-				<a href={linkedin} target="_blank">
-					Linkedin
-				</a>
-				<a href={github} target="_blank">
-					Github
-				</a>
+				<div className="header-content">
+					<p>{title}</p>
+					<p>{location}</p>
+					<a href={`mailto:${email}`}>{email}</a>
+					<a href={`tel:${phone}`}>+{phone}</a>
+					<a href={linkedin} target="_blank">
+						Linkedin
+					</a>
+					<a href={github} target="_blank">
+						Github
+					</a>
+				</div>
 			</div>
 
-			<p className="header-summary">{summary}</p>
+			<button className="summary-btn" onClick={onClickSummary}>
+				{hideSummary ? "Show summary" : "Hide summary"}
+			</button>
+			{!hideSummary && (
+				<div className="header-summary">
+					<p className="summary-text">{summary}</p>
+				</div>
+			)}
 		</section>
 	);
 };
