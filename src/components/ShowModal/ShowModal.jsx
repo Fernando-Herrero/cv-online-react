ece7a738dbc67a3a5dde8569db617a04e0cbf1a7
Fernando Herrero <herrerofernando.94@gmail.com>
2025-07-23 13:48:32 +0200

    feature(show-modal): saving before pull from develop

--- src/components/ShowModal/ShowModal.jsx
+++ src/components/ShowModal/ShowModal.jsx
@@ -7,11 +7,36 @@ export const ShowModal = ({ showModal, setShowModal, selectedItems, activeTab, c
 		Object.values(category).some((isSelected) => isSelected)
 	);
 
+	const renderExperienceAndEducation = (section) => {
+		const sectionData = selectedItems[section];
+
+		if (!sectionData || Object.keys(sectionData).length === 0) return;
+
+		return (
+			<section className="resume-section">
+				{section === "experience" ? <h2>Professional Experience</h2> : <h2>Education</h2>}
+
+				<ul>
+					{Object.entries(sectionData).map(([key, value]) => (
+						<li key={key}>{value}</li>
+					))}
+				</ul>
+			</section>
+		);
+	};
 	return (
 		<section className="show-modal-container">
 			{!hasSelectedItems && <p>There isn't any item selected</p>}
 			{hasSelectedItems && (
-				<Header activeTab={activeTab} personalInfo={cvData.personalInfo} summary={cvData.summary} />
+				<div>
+					<Header activeTab={activeTab} personalInfo={cvData.personalInfo} summary={cvData.summary} />
+					{selectedItems.experience &&
+						Object.values(selectedItems.experience).length > 0 &&
+						renderExperienceAndEducation("experience")}
+					{selectedItems.education &&
+						Object.values(selectedItems.education).length > 0 &&
+						renderExperienceAndEducation("education")}
+				</div>
 			)}
 			<ResumeButton showModal={showModal} setShowModal={setShowModal} />
 		</section>
